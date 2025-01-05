eefa918867ce12c5946f5bb7aaa8202eea8a0e0f
rajarshii82 <rajarshib13@gmail.com>
2025-01-04 21:00:17 -0500

    Again

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,11 @@
 # scripts/before_install.sh
 #!/bin/bash
 # Create application directory if it doesn't exist
+rm -rf /home/ec2-user/nextjs-app
 mkdir -p /home/ec2-user/nextjs-app
 rm -rf /home/ec2-user/nextjs-app/*
 rm -rf /home/ec2-user/nextjs-app/.next
+
 
 # Install correct Node.js version (adjust version as needed)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
